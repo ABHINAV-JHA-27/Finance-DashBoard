003d1f5aec83865f4264ba51811db81b94bcc8d3
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2023-05-14 00:49:44 +0530

    Added Configurations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,37 @@
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { themeSettings } from "./theme";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "@/scene/NavBar";
+import DashBoard from "@/scene/DashBoard";
+import Predictions from "@/scene/Predictions";
+
 function App() {
-    return <div className="app"></div>;
+    const theme = useMemo(() => createTheme(themeSettings), []);
+
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Box
+                        width="100%"
+                        height="100%"
+                        padding="1rem 2rem 4rem 2rem"
+                    >
+                        <NavBar />
+                        <Routes>
+                            <Route path="/" element={<DashBoard />} />
+                            <Route
+                                path="/predictions"
+                                element={<Predictions />}
+                            />
+                        </Routes>
+                    </Box>
+                </ThemeProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
